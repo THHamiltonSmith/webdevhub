5d22e4e11acef7ce4caa0fcf99b542bb28ace894
Akhil Galla <akhilgalla41@gmail.com>
2022-06-26 22:11:38 +0530

    add preview image functionality after uploading file

---
+++ image-convertor/imageConvertor.js
@@ -0,0 +1,24 @@
+const fileInput = document.getElementById("loadfile");
+const previewImage = document.getElementById("preview-image");
+const previewDefaultText = document.getElementById("preview-text");
+
+fileInput.addEventListener("change", function(){
+    const file = this.files[0];
+
+    if (file) {
+        const reader = new FileReader();
+
+        previewDefaultText.style.display = "none";
+        previewImage.style.display = "block";
+
+        reader.addEventListener("load", function(){
+            previewImage.setAttribute("src", this.result);
+        })
+
+        reader.readAsDataURL(file);
+    } else {
+        previewDefaultText.style.display = null;
+        previewImage.style.display = null;
+        previewImage.setAttribute("src", "");
+    }
+})
