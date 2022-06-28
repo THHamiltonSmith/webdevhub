6bb2185609ecfee5856752659609313d93bbaf18
Thomas Hamilton-Smith <thsskyenterprises@outlook.com>
2022-06-28 23:27:20 +0930

    Added HTML, CSS & JS Formatters

---
+++ code-formatter/code-formatter.js
@@ -0,0 +1,69 @@
+// code-formatter.js
+
+// References
+const codeEditorObj = document.getElementById("code-editor");
+const formattedCodeObj = document.getElementById("code-formatted");
+const formatSlector = document.getElementById("format-selector");
+
+// Get the code editors and the active code type.
+var codeEditor;
+var codeFormatted;
+var activeCodeType = "javascript";
+
+// On page load initialise the text areas.
+window.addEventListener("load", () => {
+    // Check if the URL contains a '#' (means that they clicked on a specific language on home page)
+    // Then set the modes of the code editors and language to be that language.
+    if (window.location.href.includes("#")) {
+        const lastHash = window.location.href.lastIndexOf("#");
+        const result = window.location.href.substring(lastHash + 1);
+
+        formatSlector.value = result;
+        activeCodeType = result;
+    }
+
+    // Initialise text areas.
+    codeEditor = CodeMirror.fromTextArea(codeEditorObj, {
+        mode: activeCodeType,
+        theme: "lesser-dark",
+        scrollbarStyle: "simple",
+        lineNumbers: true,
+    });
+    codeFormatted = CodeMirror.fromTextArea(formattedCodeObj, {
+        mode: activeCodeType,
+        theme: "lesser-dark",
+        scrollbarStyle: "simple",
+        lineNumbers: true,
+    });
+});
+
+// Format the code and append it to the textbox.
+function formatCode() {
+    // Get the code to format
+    const toFormatCode = codeEditor.getDoc().getValue();
+    var formattedCode = "";
+
+    // Format the code depending on the language and set it to the formatted box.
+    if (activeCodeType == "html") {
+        formattedCode = html_beautify(toFormatCode);
+    } else if (activeCodeType == "css") {
+        formattedCode = css_beautify(toFormatCode);
+    } else if (activeCodeType == "javascript") {
+        formattedCode = js_beautify(toFormatCode);
+    }
+
+    // Set the formatted value to the formatted code.
+    codeFormatted.getDoc().setValue(formattedCode);
+}
+
+// Change the format modes of the code.
+function changeModes() {
+    const newMode = formatSlector.value;
+
+    // Set the format type of the code editors to the new mode.
+    codeEditor.setOption("mode", "text/" + newMode);
+    codeFormatted.setOption("mode", "text/" + newMode);
+
+    // Set the active code type.
+    activeCodeType = newMode;
+}
