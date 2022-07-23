dfc0ed224df41dc56658b1f8f7a358d02ea20ff4
PranavThampi <57982907+PranavThampi@users.noreply.github.com>
2022-07-23 21:42:20 +0530

    added the code for epoch converter

--- public/epoch-time-converter.js
+++ public/epoch-time-converter.js
@@ -1,7 +1,19 @@
+const epoch_time = document.getElementById("epoch-time");
+const converted_time = document.getElementById("converted-time");
+const popupText = document.getElementById("popup-text");
+
 function convertTime() {
-  const epoch_time = parseInt($("input.epoch-time").val());
-  let date = new Date(epoch_time * 1000);
-  converted_date = date.toLocaleString();
-  document.getElementById("converted-time").innerHTML = converted_date;
-  console.log(converted_date);
+  let date = new Date(epoch_time.value * 1000);
+  date = date.toLocaleString();
+  converted_time.value = date;
+}
+
+function copyText() {
+  navigator.clipboard.writeText(converted_time.value);
+
+  // Displays text `Copied!` for 2 seconds
+  popupText.style.opacity = 1;
+  setTimeout(function () {
+    popupText.style.opacity = 0;
+  }, 2000);
 }
